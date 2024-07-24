e1892d6c003acc42fb5049b3ed4d14255620161b
João Pedro Watson <joaopedrowatson>
2024-07-24 14:44:51 -0300

    first commit

---
+++ src/class-based-prod-consu/producer.py
@@ -0,0 +1,9 @@
+from confluent_kafka import Producer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import (
+    MessageField,
+    SerializationContext,
+    StringSerializer,
+)
+
